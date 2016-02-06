5f7d9464300ab9498422fb7cb1ff6916003eb770
wkingsnorth <wkingsnorth@users.noreply.github.com>
2016-02-06 18:02:33 +0000

    Scraper works!
    Creates a .csv - excel reads one of the lines weirdly but I can't see
    any problems - slightly worrying
    
    Exceptions still a mess - will fix

--- src/SotonMenuScraper.java
+++ src/SotonMenuScraper.java
@@ -1,40 +1,155 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.*;
 
 /**
- * Created by William on 05/02/2016.
+ * Created by Will on 05/02/2016.
  */
 public class SotonMenuScraper {
 
-    HashMap<String, String> outlets;
-
-    public SotonMenuScraper(){
-
-    }
-
-    // Reads URLs and venues from "Venues.txt" - "Arlott,[urlgoeshere]" gets stored in outlets as Arlott and the url
-    public void getVenuesURLs() throws IOException {
-        BufferedReader br = null;
+    private HashMap<String, URL> outlets;
+    private ArrayList<String> foods;
+    private LocalDate today;
+    // TODO: Exceptions are a mess
+    public static void main(String[] args) {
         try {
-            br = new BufferedReader(new FileReader("Venues.txt"));
-        } catch (FileNotFoundException e) {
-            System.err.println("Venues.txt not found, please ensure it exists");
+            SotonMenuScraper sms = new SotonMenuScraper();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        String myString;
-        String venue;
-        String url;
-        // Splits URL and stores
-        while(br.ready()){
-            myString = br.readLine();
-            venue = myString.split(",")[0];
-            url = myString.split(",")[1];
-            outlets.put(venue, url);
 
+
+    }
+
+    public SotonMenuScraper() throws IOException {
+        today = LocalDate.now();
+        // Here for weekend coding - remove from production
+        today = today.minusDays(1);
+        outlets = getVenuesURLs();
+        foods = new ArrayList<String>();
+        Iterator it = outlets.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry<String, URL> entry = (Map.Entry<String, URL>)it.next();
+            foods.addAll(venueScraper(entry.getValue()));
+        }
+        it = foods.iterator();
+        //while(it.hasNext()){
+            //System.out.println(it.next());
+        //}
+        createCSV("themightycsvoffood.csv");
+
+    }
+
+    // Creates URLs and stores them in outlets hasmap
+    private HashMap<String, URL> getVenuesURLs() throws IOException {
+        HashMap<String, URL> myOutlets = new HashMap<String, URL>();
+        URL terraceURL = new URL("http://data.southampton.ac.uk/dumps/catering-daily-menu/" + today.toString() + "/todays-menu-38-terrace.csv");
+        URL piazzaURL = new URL("http://data.southampton.ac.uk/dumps/catering-daily-menu/" + today.toString() + "/todays-menu-42-piazza.csv");
+        myOutlets.put("Terrace", terraceURL);
+        myOutlets.put("Piazza", piazzaURL);
+        return myOutlets;
+    }
+
+    // Reads data from URL passed to it and adds food to foods in format location,food,price
+    private ArrayList<String> venueScraper(URL venueURL){
+        ArrayList<String> foodList = new ArrayList<>();
+        try {
+
+            Scanner vsc = new Scanner(venueURL.openStream());
+            String nowLine, lastValue, foodPrice, foodName, foodCategory, foodVenue, outputLine;
+            String[] nowLineContents;
+            vsc.nextLine(); // Skips over first line with headers
+            while(vsc.hasNext()){
+                nowLine = vsc.nextLine();
+                nowLineContents = nowLine.split(",");
+                lastValue = nowLineContents[nowLineContents.length - 1];
+                if(!lastValue.contains("£")){
+                    nowLine = nowLine + vsc.nextLine();
+                }
+                foodPrice = getFoodPrice(nowLine);
+                foodName = getFoodName(nowLine);
+                foodCategory = getFoodCat(nowLine);
+                foodVenue = getFoodVenue(nowLine);
+                if(!foodPrice.equals("")) {
+                    outputLine = foodVenue + "," + foodCategory + "," + foodName + "," + foodPrice;
+                    //System.err.println(outputLine);
+                    foods.add(outputLine);
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("The URL for the menu broke");
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            System.err.println("The URL for the menu broke");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return foodList;
+
+    }
+
+    private String getFoodName(String line){
+        return line.split(",")[2];
+    }
+
+    private String getFoodCat(String line){
+        return line.split(",")[1];
+    }
+
+    private String getFoodPrice(String line){
+        String price = line.split(",")[line.split(",").length - 2];
+        if(price.length() < 2){
+            return "";
+        }
+        //Pence case
+        if(price.substring(price.length() - 1, price.length()).equals("p")){
+            if(price.length() == 2){
+                price = price.substring(0, 1);
+                price = "£0.0" + price;
+            } else if(price.length() == 3){
+                price = price.substring(0, 2);
+                price = "£0." + price;
+            } else {
+                return "";
+            }
+        }
+        if(!price.substring(0, 1).equals("£")){
+            return "";
+        }
+        return price;
+    }
+
+    private String getFoodVenue(String line){
+        String firstValue = line.split(",")[0];
+        return firstValue.split("-")[1];
+    }
+
+
+    public void createCSV(String fileName){
+        Iterator<String> it = foods.iterator();
+        try {
+            PrintStream csv = new PrintStream(fileName);
+            while(it.hasNext()){
+                String myStr = it.next();
+                csv.println(myStr);
+                System.out.println(myStr);
+
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
+
+    public ArrayList<String> getFoodList(){
+        return foods;
+    }
+
+
 
 }
